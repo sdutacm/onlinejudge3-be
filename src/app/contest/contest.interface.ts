67c8cfd52b76f2600fa90458b5ef670d500145f2
bLue <tbdblue@gmail.com>
2020-06-17 08:21:09 +0800

    A: contest interface

---
+++ src/app/contest/contest.interface.ts
@@ -0,0 +1,59 @@
+export interface IContestModel {
+  contestId: number;
+  title: string;
+  description: string;
+  intro: string;
+  type: number;
+  category: number;
+  mode: number;
+  password: string;
+  startAt: Date;
+  endAt: Date;
+  frozenLength: number;
+  registerStartAt: Date | null;
+  registerEndAt: Date | null;
+  team: boolean;
+  end: boolean;
+  hidden: boolean;
+}
+
+export type TContestModelFields = keyof IContestModel;
+
+export type TMContestLiteFields = Extract<
+  TContestModelFields,
+  | 'contestId'
+  | 'title'
+  | 'type'
+  | 'category'
+  | 'mode'
+  | 'startAt'
+  | 'endAt'
+  | 'frozenLength'
+  | 'registerStartAt'
+  | 'registerEndAt'
+  | 'team'
+  | 'ended'
+>;
+
+export type TMContestDetailFields = Extract<
+  TContestModelFields,
+  | 'contestId'
+  | 'title'
+  | 'type'
+  | 'category'
+  | 'mode'
+  | 'intro'
+  | 'description'
+  | 'password'
+  | 'startAt'
+  | 'endAt'
+  | 'frozenLength'
+  | 'registerStartAt'
+  | 'registerEndAt'
+  | 'team'
+  | 'ended'
+>;
+
+export type IMContestLite = Pick<IContestModel, TMContestLiteFields>;
+export type IMContestDetail = Pick<IContestModel, TMContestDetailFields>;
+export type IMContestListPagination = defService.ServiceListOpt<TContestModelFields>;
