4be279913d5cf0f24505f1f9bd1629ec069f0adc
bLue <tbdblue@gmail.com>
2020-07-02 10:26:25 +0800

    U: solution interface

--- src/app/solution/solution.interface.ts
+++ src/app/solution/solution.interface.ts
@@ -1,3 +1,7 @@
+import { IUserModel } from '../user/user.interface';
+import { IProblemModel } from '../problem/problem.interface';
+import { IContestModel } from '../contest/contest.interface';
+
 export interface ISolutionModel {
   solutionId: number;
   problemId: number;
@@ -46,5 +50,64 @@ export type TMSolutionDetailFields = Extract<
   | 'createdAt'
 >;
 
-export type IMSolutionLite = Pick<ISolutionModel, TMSolutionLiteFields>;
-export type IMSolutionDetail = Pick<ISolutionModel, TMSolutionDetailFields>;
+export type IMSolutionRelativeProblem = Pick<IProblemModel, 'problemId' | 'title' | 'timeLimit'>;
+export type IMSolutionRelativeUser = Pick<
+  IUserModel,
+  'userId' | 'username' | 'nickname' | 'avatar' | 'bannerImage' | 'rating'
+>;
+export type IMSolutionRelativeContest = Pick<IContestModel, 'contestId' | 'title' | 'type'>;
+
+export type IMSolutionLite = Omit<
+  Pick<ISolutionModel, TMSolutionLiteFields>,
+  'problemId' | 'userId' | 'contestId'
+> & {
+  problem: IMSolutionRelativeProblem;
+} & {
+  user: IMSolutionRelativeUser;
+} & {
+  contest?: IMSolutionRelativeContest;
+};
+export type IMSolutionDetail = Omit<
+  Pick<ISolutionModel, TMSolutionDetailFields>,
+  'problemId' | 'userId' | 'contestId'
+> & {
+  problem: IMSolutionRelativeProblem;
+} & {
+  user: IMSolutionRelativeUser;
+} & {
+  contest?: IMSolutionRelativeContest;
+};
+
+//#region service.getList
+export interface IMSolutionServiceGetListOpt {
+  solutionId?: ISolutionModel['solutionId'];
+  solutionIds?: ISolutionModel['solutionId'][];
+  problemId?: ISolutionModel['problemId'];
+  userId?: ISolutionModel['userId'];
+  contestId?: ISolutionModel['contestId'];
+  result?: ISolutionModel['result'];
+  language?: ISolutionModel['language'];
+}
+
+export type IMSolutionServiceGetListRes = defModel.ListModelRes<IMSolutionLite>;
+//#endregion
+
+//#region service.getDetail
+export type IMSolutionServiceGetDetailRes = defModel.DetailModelRes<IMSolutionDetail>;
+//#endregion
+
+//#region service.getRelative
+export type IMSolutionServiceGetRelativeRes = Record<
+  ISolutionModel['solutionId'],
+  IMSolutionDetail
+>;
+//#endregion
+
+//#region service.findOne
+export type IMSolutionServiceFindOneOpt = Partial<ISolutionModel>;
+export type IMSolutionServiceFindOneRes = defModel.DetailModelRes<IMSolutionDetail>;
+//#endregion
+
+//#region service.isExists
+export type IMSolutionServiceIsExistsOpt = Partial<ISolutionModel>;
+//#endregion
