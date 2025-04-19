8cea37126bf95fb1fdbc783a463ebfdca0941c8c
bLue <tbdblue@gmail.com>
2025-04-20 04:05:06 +0800

    chore: update duration config

--- src/config/durations.config.ts
+++ src/config/durations.config.ts
@@ -1,11 +1,11 @@
 const durations = {
-  cacheDetail: 86400, // s
+  cacheDetail: 3600, // s
   cacheDetailVeryShort: 15, // s
   cacheDetailShort: 30, // s
   cacheDetailMedium: 60, // s
   cacheDetailLong: 300, // s
   cacheDetailNull: 10, // s
-  cacheFullList: 86400, // s
+  cacheFullList: 7200, // s
   emailVerificationCodeExpires: 86400, // s
   emailVerificationCodeRetryAfter: 60, // s
 };
